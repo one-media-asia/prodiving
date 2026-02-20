6b785d73c0f6e38790d2de1eae4e69991c13364c
prodiving <onemediaasia@duck.com>
2026-02-21 07:16:26 +0800

    s

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => (
+  <div style={{ padding: "20px", backgroundColor: "lightblue", minHeight: "100vh" }}>
+    <h1>Pro Diving Site is Working!</h1>
+    <p>The React app is now loading properly.</p>
+  </div>
+);
+
+export default App;
