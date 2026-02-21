07416a073848c7c047257a8f6df2a1a0fb9983ff
prodiving <onemediaasia@duck.com>
2026-02-21 08:08:06 +0800

    sd

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App.tsx";
-import "./index.css";
 
-console.log("Main.tsx loaded");
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
 const container = document.getElementById("root");
 if (container) {
-  try {
-    ReactDOM.render(<App />, container);
-    console.log("App rendered successfully");
-  } catch (error) {
-    console.error("Error rendering App:", error);
-    container.innerHTML = "<h1 style='color: red;'>Error loading React app</h1>";
-  }
-} else {
-  console.error("Root element not found");
+  const root = createRoot(container);
+  root.render(<App />);
 }
-
-<script crossorigin src="https://unpkg.com/react@18/umd/react.development.js"></script>
-<script crossorigin src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
